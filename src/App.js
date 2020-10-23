3f3770432dab131e948bd3ee40907329ae31dd7a
Caleb Denio <cjdenio44@gmail.com>
2020-10-23 12:47:36 -0400

    :tada: Initialize React app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="header">
+        <div class="header-info">
+          <img
+            src="https://a.slack-edge.com/80588/img/apps/default_new_app_icon.png"
+            class="app-icon"
+          />
+          <div class="title-and-star">
+            <div class="title">
+              <span class="title-span">Slack Uno</span>
+            </div>
+          </div>
+        </div>
+        <div class="spacer"></div>
+        <div>hi</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
